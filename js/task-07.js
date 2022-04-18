750a1a0601e49319e4c53121999346e3deb81478
Maksym Pshenychnyi <maksim_pshenichniy@mail.ru>
2022-04-18 16:47:20 +0300

    fix 7 hometask

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,8 @@
 const inputEl = document.querySelector('#font-size-control');
 const outputEl = document.querySelector('#text');
 
+outputEl.style.fontSize = inputEl.value + 'px';
+
 inputEl.addEventListener('input', onInput);
 
 function onInput(event) {
